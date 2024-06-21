ef8789992fb48c5aed329a15c4fca95e661d70cf
shahab <shahaab.kabiri@gmail.com>
2024-06-20 21:21:56 -0700

    :rocket: USER-change pass url view

--- backend/apps/accounts/urls/auth.py
+++ backend/apps/accounts/urls/auth.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from apps.accounts.views import auth
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("login/", auth.LoginView.as_view(), name="login"),
@@ -11,5 +12,18 @@ urlpatterns = [
     path("register/", auth.UserRegisterView.as_view(), name="registration"),
     path("logout/", auth.LogoutView.as_view(), name="logout"),
     path("user_edit/", auth.edit_user, name="edit_account"),
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(
+            success_url="password_change",
+            template_name="public/profile-editpassword.html",
+        ),
+        name="password_change",
+    ),
+    path(
+        "password-change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
     # (auth)/forget-password
 ]
